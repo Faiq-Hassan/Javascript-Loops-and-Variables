2fc95cbdc4fd6f64b4f423c8c4e3fbbb3cfe3e6d
Faiq-Hassan <faiqghumman4@gmail.com>
2023-09-05 17:23:13 +0500

    updated

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = 34;
+        var b = "harry";
+        var c = 33.44;
+        console.log('c');
+        // operators
+        c = -c;
+        console.log(c);
+        c = 33 + 2;
+        console.log('c');
